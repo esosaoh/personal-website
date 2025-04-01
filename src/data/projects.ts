26de3bcd3e1f97d574ba7be24267c702ae6c28e9
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-04-01 18:09:30 -0400

    fix images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,41 +3,41 @@ export const projects = [
     {
       title: 'GitMentor',
       description: 'An AI-assistant for open-source contributors',
-      mediaUrl: '/GitMentor.webp', 
+      mediaUrl: '/personal-website/GitMentor.webp', 
       githubUrl: 'https://github.com/esosaoh/git-mentor'
     },
     {
       title: 'ListenUP',
       description: 'Seamless playlist migration from Spotify to YouTube Music',
-      mediaUrl: '/ListenUp.webp', 
+      mediaUrl: '/personal-website/ListenUp.webp', 
       githubUrl: 'https://github.com/esosaoh/ListenUP'
     },
     {
       title: 'Compressr',
       description: 'An implementation of Huffman encoding algorithm for file compression using a custom bit-stream approach',
-      mediaUrl: '/compressr.webp', 
+      mediaUrl: '/personal-website/compressr.webp', 
       githubUrl: 'https://github.com/esosaoh/compressr'
     },
     {
       title: 'Kubewarden (Open-source)',
       description: 'A WebAssembly-powered Kubernetes policy engine with 1.3K+ stars on GitHub.',
-      mediaUrl: '/kubewarden.webp', 
+      mediaUrl: '/personal-website/kubewarden.webp', 
     },
     {
       title: 'Carleton Courses',
       description: 'A redesigned student portal with improved course search and scheduling features.',
-      mediaUrl: '/carletoncourses.webp', 
+      mediaUrl: '/personal-website/carletoncourses.webp', 
       githubUrl: 'https://github.com/esosaoh/hack-the-tunnels'
     },
     {
       title: 'Sorting Visualizer',
       description: 'An interactive tool that brings sorting algorithms to life with animations.', 
-      mediaUrl: '/sort.webp'
+      mediaUrl: '/personal-website/sort.webp'
     },
   ];
   
   export const personalInfo = {
     name: "Esosa Ohangbon",
-    photoUrl: "/profilephoto.webp",
+    photoUrl: "/personal-website/profilephoto.webp",
     about: "I'm a sophomore Software Engineering student at Carleton University. I'm interested in full-stack development, AI, infrastructure, distributed systems, compilers and open-source projects."
   };
