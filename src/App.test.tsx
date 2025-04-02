9fc76eca57c44c24f3445a6cc155238fc951da12
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-04-01 20:43:35 -0400

    fix bugs

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { personalInfo } from './data/projects';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders profile name', () => {
+  render(<App profile={personalInfo} />);
+  const nameElement = screen.getByText(personalInfo.name);
+  expect(nameElement).toBeInTheDocument();
 });
